e9ae6f8ac1c1ef50ddc89c966232f621aa98a849
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2025-03-29 20:43:52 +0500

    set package json file////;;

--- index.js
+++ index.js
@@ -57,8 +57,8 @@ app.get('/home' ,(req,res)=>{
  
 
 
-const PORT = process.env.PORT||5000;
-app.listen(PORT, () => {
-console.log(`Server is running on port ${PORT}`   )},
-);
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server running at http://0.0.0.0:${PORT}`);
+});
 
